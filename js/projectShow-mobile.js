cea908aec56b1e847b14b192baa7ba46b8e489b1
Zeinab <zeinabansarian6280@gmail.com>
2024-08-29 09:14:56 +0330

    complete news list , productList and projectShow pages

---
+++ js/projectShow-mobile.js
@@ -0,0 +1,21 @@
+const relatedProduct = new Swiper('.relatedProduct', {
+
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.nextRelatePro',
+    prevEl: '.prevRelatePro',
+  },
+
+
+});
+const relatedProject = new Swiper('.relatedProject', {
+
+  loop: false,
+  spaceBetween: 10,
+
+
+
+
+});
